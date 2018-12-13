846b5ac87605da97951ba75c00d194ab38ce8f2d
piouswolf <piouswolf@gmail.com>
2018-12-13 11:02:22 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,8 +1,8 @@
 local emojiFilter = require "emojiFilter"
 local strWithEmoji = "Hello,❄World!";
 local strTrimed = emojiFilter:trimEmoji(strWithEmoji);
-print("strTrimed", strTrimed);
+print("strTrimed:", strTrimed);
 
 ----------------------------printed------------------------------
---strTrimed Hello,World!
+--strTrimed:  Hello,World!
 -----------------------------------------------------------------
